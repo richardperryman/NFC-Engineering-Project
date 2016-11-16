b3375b975fdb80ecb4d4c2c3ba1732843fe05650
richardperryman <richard.jperryman@gmail.com>
2016-11-15 21:21:21 -0500

    Updated app to do slightly more complicated api calls

--- Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/MainActivity.java
+++ Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sbacs.phoneapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,22 +15,12 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.SBACS.app.MESSAGE";
     public final static String USER_ID = "com.SBACS.app.USER_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    private Response.Listener<String> genericResponseListener(final TextView view) {
-        return new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                view.setText(response);
-            }
-        };
     }
 
     private Response.ErrorListener genericErrorListener(final TextView view) {
@@ -41,40 +32,33 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    public void pingServer(View view) {
-        TextView results = (TextView) findViewById(R.id.result_message);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = getResources().getString(R.string.sbacs_url);
-        StringRequest request = new StringRequest(Request.Method.GET, url, genericResponseListener(results),
-                genericErrorListener(results));
-        queue.add(request);
-    }
-
-    public void getUsers(View view) {
-        TextView results = (TextView) findViewById(R.id.result_message);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = getResources().getString(R.string.sbacs_url) +
-                getResources().getString(R.string.user_table_name);
-        StringRequest request = new StringRequest(Request.Method.GET, url, genericResponseListener(results),
-                genericErrorListener(results));
-        queue.add(request);
-    }
-
-    public void getLocks(View view) {
-        TextView results = (TextView) findViewById(R.id.result_message);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = getResources().getString(R.string.sbacs_url) +
-                getResources().getString(R.string.lock_table_name);
-        StringRequest request = new StringRequest(Request.Method.GET, url, genericResponseListener(results),
-                genericErrorListener(results));
-        queue.add(request);
-    }
-
     public void login(View view) {
-        Intent loginIntent = new Intent(this, UserActivity.class);
-        String message = "I did a success: ";
-        loginIntent.putExtra(EXTRA_MESSAGE, message);
-        loginIntent.putExtra(USER_ID, 1);
-        startActivity(loginIntent);
+        TextView results = (TextView) findViewById(R.id.result_message);
+        String user_name = ((TextView) findViewById(R.id.user_name)).getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = getResources().getString(R.string.sbacs_url) +
+                getResources().getString(R.string.user_table_name) +
+                "?name=" + user_name;
+        final Context currentContext = this;
+        Response.Listener<String> loginResponseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int user_id;
+                try {
+                    // This probably shouldn't be hardcoded!
+                    user_id = Integer.parseInt(response.split("\\s+")[0]);
+                } catch (NumberFormatException e) {
+                    user_id = -1;
+                }
+
+                Intent loginIntent = new Intent(currentContext, UserActivity.class);
+                loginIntent.putExtra(USER_ID, user_id);
+                startActivity(loginIntent);
+            }
+        };
+        StringRequest request = new StringRequest(Request.Method.GET, url, loginResponseListener,
+                genericErrorListener(results));
+        queue.add(request);
     }
 }
