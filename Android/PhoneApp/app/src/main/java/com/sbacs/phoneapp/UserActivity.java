5dea9a3b8b8cd908e774d7f5ccde956896bf5b2e
richardperryman <richard.jperryman@gmail.com>
2016-11-22 18:40:48 -0500

    Added first attempt at host-based card emulation

--- Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/UserActivity.java
+++ Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/UserActivity.java
@@ -1,5 +1,6 @@
 package com.sbacs.phoneapp;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -29,10 +30,27 @@ public class UserActivity extends AppCompatActivity {
         Intent intent = getIntent();
         user_id = intent.getIntExtra(MainActivity.USER_ID, -1);
 
+        // This currently ties the service to this window being open... this should probably not be the case
+        this.startService(new Intent(this, SBACSNFCService.class));
+
         ListView view = (ListView) findViewById(R.id.list_results);
-        List<String> resultsList = new ArrayList<String>();
+        List<String> resultsList = new ArrayList<>();
         resultsList.add("Nothing to see here");
+        // Left in just to make testing tomorrow easier
+        if (checkSBACVSServiceRunning()) {
+            resultsList.add("SBACS Service is operational");
+        }
         view.setAdapter(new ArrayAdapter<>(this, R.layout.adapter_text_view, resultsList));
+    }
+
+    private boolean checkSBACVSServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (service.service.getClassName().equals(SBACSNFCService.class.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private Response.ErrorListener genericErrorListener() {
