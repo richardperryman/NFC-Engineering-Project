d1699b75999bb3c8ed2d48617253af0168d8d0e2
richardperryman <richard.jperryman@gmail.com>
2016-11-27 20:18:51 -0500

    Added changes to make service print out APDUs for testing

--- Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/SBACSNFCService.java
+++ Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/SBACSNFCService.java
@@ -1,16 +1,39 @@
 package com.sbacs.phoneapp;
 
+import android.content.Intent;
 import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+
 import java.util.Arrays;
 
 public class SBACSNFCService extends HostApduService {
+    public final static String SBACS_NOTIFICATION = "com.SBACS.service.NOTIFICATION";
+    public final static String SBACS_MESSAGE = "com.SBACS.service.MESSAGE";
+
     private final static byte[] data_message = "Hi Jess!".getBytes();
     private final static byte[] success_message = new byte[]{0};
     private final static byte sentinel_byte = 0x00;
+
+    private LocalBroadcastManager broadcaster;
+
+    @Override
+    public void onCreate() {
+        broadcaster = LocalBroadcastManager.getInstance(this);
+        // Just for testing
+        Intent notifyIntent = new Intent(SBACS_NOTIFICATION);
+        notifyIntent.putExtra(SBACS_MESSAGE, "Test message");
+        broadcaster.sendBroadcast(notifyIntent);
+    }
+
     @Override
     public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
         // If this becomes expensive use another thread and use sendResponseApdu()
+
+        Intent notifyIntent = new Intent(SBACS_NOTIFICATION);
+        notifyIntent.putExtra(SBACS_MESSAGE, apdu.toString());
+        broadcaster.sendBroadcast(notifyIntent);
+
         if (apdu.length == 0) {
             return data_message;
         } else if (apdu[0] == sentinel_byte) {
--- Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/UserActivity.java
+++ Android/PhoneApp/app/src/main/java/com/sbacs/phoneapp/UserActivity.java
@@ -1,8 +1,11 @@
 package com.sbacs.phoneapp;
 
 import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,8 @@ import java.util.List;
 public class UserActivity extends AppCompatActivity {
     private int user_id;
 
+    private BroadcastReceiver receiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,26 +36,27 @@ public class UserActivity extends AppCompatActivity {
         user_id = intent.getIntExtra(MainActivity.USER_ID, -1);
 
         // This currently ties the service to this window being open... this should probably not be the case
-        this.startService(new Intent(this, SBACSNFCService.class));
+        Intent serviceIntent = new Intent(this, SBACSNFCService.class);
+        this.startService(serviceIntent);
 
-        ListView view = (ListView) findViewById(R.id.list_results);
+        final ListView view = (ListView) findViewById(R.id.list_results);
         List<String> resultsList = new ArrayList<>();
         resultsList.add("Nothing to see here");
-        // Left in just to make testing tomorrow easier
-        if (checkSBACVSServiceRunning()) {
-            resultsList.add("SBACS Service is operational");
-        }
         view.setAdapter(new ArrayAdapter<>(this, R.layout.adapter_text_view, resultsList));
+
+        //
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                ((ArrayAdapter<String>)view.getAdapter()).add(intent.getStringExtra(SBACSNFCService.SBACS_MESSAGE));
+            }
+        };
     }
 
-    private boolean checkSBACVSServiceRunning() {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (service.service.getClassName().equals(SBACSNFCService.class.getName())) {
-                return true;
-            }
-        }
-        return false;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(SBACSNFCService.SBACS_NOTIFICATION));
     }
 
     private Response.ErrorListener genericErrorListener() {
