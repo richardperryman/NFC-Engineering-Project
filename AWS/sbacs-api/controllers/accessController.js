276013c6bd97f1e392df9708aaa830be6079af74
Craig Shorrocks <craigshorrocks@cmail.carleton.com>
2016-11-29 10:45:49 -0500

    Modified access controller to be framework for new functionality

--- AWS/sbacs-api/controllers/accessController.js
+++ AWS/sbacs-api/controllers/accessController.js
@@ -34,46 +34,29 @@ handleRequest : function(req,res,db_helper) {
 
 
 function handleGet(req,res) {
-	// TODO Write the authenticator set GET method
-}
-
-function handlePost(req,res){
 	var parsedRequest = url.parse(req.url, true);
 	
-	// Extract information that is needed
 	var lock_id = parsedRequest.query['lock_id'];
-	var given_auth = parsedRequest.query['auth'];
-	
 	if(lock_id == undefined){
-		console.log('No lock provided for access');
+		console.log('No lock id provided');
 		res.writeHead(500);
 		res.write('Lock_Id not provided');
 		res.end();
 		return;
-	} else if (given_auth == undefined){
-		console.log('No authenticators provided for access');
-		res.writeHead(500);
-		res.write('Authenticators not provided');
-		res.end();
-		return;
 	}
+	
 	var inserts = [lock_id];
 	var queryString = mysql.format(QUERY_GET_NEEDED_AUTH, inserts);
-	
 	// Execute query to get needed auth
 	db.performQuery(queryString, function(err,rows,fields){
 		if(!err){
-			// TODO: Check the needed against given, then do necessary
-			var needed_auth = extractAuthenticators(rows,fields);
-			if(checkAuth(given_auth, needed_auth)){
-				res.writeHead(200);
-				res.write('SuccessCode');
-				res.end();
-			} else {
-				res.writeHead(401);
-				res.write('Access Denied');
-				res.end();
-			}
+			// Extract authenticator info
+			var needed_auth = extractAuthenticatorsFromDb(rows,fields);
+			var minimal_auth_set = getMinimalAuthSet(needed_auth);
+			// Send results back
+			res.writeHead(200);
+			res.write(minimal_auth_set);
+			res.end();
 		} else {
 			// Handle error
 			console.log('Error with DB');
@@ -84,6 +67,70 @@ function handlePost(req,res){
 	});
 }
 
+function handlePost(req,res){
+	var parsedRequest = url.parse(req.url, true);
+	
+	// Extract information that is needed
+	var lock_id = parsedRequest.query['lock_id'];
+	var lock_key = parsedRequest.query['lock_key'];
+	
+	if(lock_id == undefined){
+		console.log('No lock provided for access');
+		res.writeHead(500);
+		res.write('Lock_Id not provided');
+		res.end();
+		return;
+	} else if (lock_key == undefined){
+		console.log('No key provided for access');
+		res.writeHead(500);
+		res.write('Lock key not provided');
+		res.end();
+		return;
+	}
+	
+	var inserts = [lock_id];
+	var queryString = mysql.format(QUERY_GET_NEEDED_AUTH, inserts);
+	// Execute query to get needed auth
+	db.performQuery(queryString, function(err,rows,fields){
+		if(!err){
+			// Check the needed against given, then do necessary
+			var needed_auth = extractAuthenticatorsFromDb(rows,fields);
+			
+			// Get body and process Authenticators
+			var body = [];
+			req.on('data', function(chunk) {
+				body.push(chunk);
+			}).on('end', function () {
+				body = Buffer.concat(body).toString();
+				
+				var given_auth = extractAuthenticatorsFromBody(body);
+				// Check given and needed authenticators
+				if(checkAuth(given_auth, needed_auth)){
+					res.writeHead(200);
+					res.write('SuccessCode');
+					res.end();
+				} else {
+					res.writeHead(401);
+					res.write('Access Denied');
+					res.end();
+				}
+			});
+		} else {
+			// Handle error
+			console.log('Error with DB');
+			res.writeHead(500);
+			res.write('An error occurred.');
+			res.end();
+		}
+	});
+}
+
+// Gets the minimal set of authenticators needed for the given list
+function getMinimalAuthSet(all_auth) {
+	// TODO: Implement this function
+	
+	return 'blah';
+}
 
 // Checks if the given and needed authenticators are equal
 //	 returns true if equal, otherwise false
@@ -98,7 +145,14 @@ function checkAuth(given, needed){
 }
 
 // Extracts the authenticators from DB into useful format
-function extractAuthenticators(rows,fields) {
+function extractAuthenticatorsFromDb(rows,fields) {
+	// TODO: Implement this function
+	
+	return 'blah';
+}
+
+// Extracts the authenticators from Body info useful format
+function extractAuthenticatorsFromBody(body){
 	// TODO: Implement this function
 	
 	return 'blah';
