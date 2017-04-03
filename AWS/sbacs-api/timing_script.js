f7a46e1051a5ebf1799093cc822e4d71c9e3f964
richardperryman <richard.jperryman@gmail.com>
2017-04-02 20:26:27 -0400

    Added another table and made the testing script a bit more robust

--- AWS/sbacs-api/timing_script.js
+++ AWS/sbacs-api/timing_script.js
@@ -8,13 +8,16 @@ function time_pbkdf2(iterations, algorithm) {
 
 }
 
-const algorithms = ["sha256", "sha1"];
+const algorithms = ["sha256"];
 
 for(var algo = 0; algo < algorithms.length; algo++) {
 	for(var i = 1; i <= 1e6; i*=10) {
-		var timer_name = "time pbkdf2 with " + i + " iterations using " + algorithms[algo];
-		console.time(timer_name);
-		time_pbkdf2(i, algorithms[algo]);
-		console.timeEnd(timer_name);
+		for(var salt_len = 1; salt_len <= 256; salt_len*=2){
+			var timer_name = "time pbkdf2 with " + i + " iterations using " + 
+				algorithms[algo] + " with salt length " + salt_len;
+			console.time(timer_name);
+			time_pbkdf2(i, algorithms[algo]);
+			console.timeEnd(timer_name);
+		}
 	}
 }
